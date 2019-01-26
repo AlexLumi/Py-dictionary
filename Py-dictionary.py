fcb89f649a7e5acbbeec45dbe30ddd1f2df71298
Alex Crosslen <40043670+AlexLumi@users.noreply.github.com>
2019-01-26 13:09:00 -0500

    Update

--- Py-dictionary.py
+++ Py-dictionary.py
@@ -1,17 +1,22 @@
 
-
 print("Coding Dictionary")
 level = input ("What level of programming are you?: ")
 level = level.lower()
 
+
 def beginner():
-    learn = input ("Hello " + level + " What do you want to learn today?")
+    learn = input ("Hello " + level + " what do you want to learn today?")
     learn = learn.strip()
     learn = learn.lower()
-    beginner_list =  {"Print": "This will print the string"}
+    beginner_list =  {"print": "This will print the string"}
     if learn in beginner_list:
-        print (beginner_list.get(learn))
+        print ( learn.title() + ": " + beginner_list.get(learn))
 
-if level == "beginner" or "novice":
+if level == "beginner" or "novice" or "potato":
     beginner()
+    choice = input("Do you want to keep researching in beginner ")
+    if choice == "yes" or "yea" or "ya":
+        beginner()
+    else:
+        print("Bye!")
 
