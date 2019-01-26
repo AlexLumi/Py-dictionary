2708a982a2308d8009968d4f7c959216963e6540
Alex Crosslen <40043670+AlexLumi@users.noreply.github.com>
2019-01-26 12:35:15 -0500

    Basic Concept

--- Py-dictionary.py
+++ Py-dictionary.py
@@ -1,7 +1,17 @@
-print ("Hello World")
-print("ok")
 
 
+print("Coding Dictionary")
+level = input ("What level of programming are you?: ")
+level = level.lower()
 
-print("memes")
+def beginner():
+    learn = input ("Hello " + level + " What do you want to learn today?")
+    learn = learn.strip()
+    learn = learn.lower()
+    beginner_list =  {"Print": "This will print the string"}
+    if learn in beginner_list:
+        print (beginner_list.get(learn))
+
+if level == "beginner" or "novice":
+    beginner()
 
