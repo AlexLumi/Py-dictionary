a288cf81c1f34eee83c01e1174b852fc023b8382
Alex Crosslen <40043670+AlexLumi@users.noreply.github.com>
2019-01-26 15:03:11 -0500

    Scraped Everything and create a general terms section

--- Py-dictionary.py
+++ Py-dictionary.py
@@ -1,22 +1,29 @@
 
-print("Coding Dictionary")
-level = input ("What level of programming are you?: ")
+print("Py Dictionary")
+level = input ("What do you want to do understand? (General Terms) ")
 level = level.lower()
 
 
-def beginner():
-    knowledge
-    learn = input ("Hello " + level + " what do you want to learn today?")
-    learn = learn.strip()
-    learn = learn.lower()
-    beginner_list =  {"print": "This will print the string"}
-    if learn in beginner_list:
-        print ( learn.title() + ": " + beginner_list.get(learn))
+def gen_terms():
+    knowledge = input("Do you want see the table of contents?(Highly Suggested) ")
+    knowledge = knowledge.lower()
+    gen_terms =  [">>>","abs","argument","argparse","assert","assignment","block","break","class","compiler","continue","if","else","elif",
+                    "def","dictionary","distutils","__future__", "easy_install","exceptions","filter","float","for","if","import","int","lambda","list","map"
+                    "object", "pass","reduce","set","setuptools","slice","str","string","try","tuple","while","with","yield"]
+    if knowledge == "yes":
+        print ("Cool! Hello, what do you want to learn today?")
+        print (gen_terms)
+        term = input()
+        term = help(term)
+        print (term)
+    else:
+        term = input("")
+        help(term)
 
-if level == "beginner" or "novice" or "potato":
-    beginner()
-    choice = input("Do you want to keep researching in beginner ")
+if level == "general term" or "general terms":
+    gen_terms()
+    choice = input("Do you want to keep researching in general terms? ")
     if choice == "yes" or "yea" or "ya":
-        beginner()
+        gen_terms()
     else:
         print("Bye!")
